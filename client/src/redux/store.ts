b3465a528401f256c0f86e3e8fa13c024ac583e6
Viktoriia <vikatori1409801@gmail.com>
2024-11-09 19:24:11 +0200

    Updated store

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authApi } from "../redux/api/authSlice";
 import { feedbackApi } from "../redux/api/feedbackSlice";
+import { jobSeekerApi } from "./api/jobseekerSlice";
+import { companyApi } from "./api/companySlice";
 
 export const store = configureStore({
   reducer: {
@@ -11,7 +13,12 @@ export const store = configureStore({
   },
   // Adding the authApi middleware to enable caching, invalidation, etc.
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(feedbackApi.middleware, authApi.middleware),
+    getDefaultMiddleware().concat(
+      feedbackApi.middleware,
+      authApi.middleware,
+      jobSeekerApi.middleware,
+      companyApi.middleware
+    ),
 });
 
 // Export types for usage with hooks
